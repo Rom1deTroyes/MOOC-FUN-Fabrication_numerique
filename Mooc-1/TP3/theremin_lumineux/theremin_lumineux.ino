ae4a0cfa077192fce81e84cc55c9c35fdd329288
Rom1deTroyes <Rom1deTroyes@users.noreply.github.com>
2017-03-05 17:36:03 +0100

    TP3 : Theremin Lumineux

--- Mooc-1/TP3/theremin_lumineux/theremin_lumineux.ino
+++ Mooc-1/TP3/theremin_lumineux/theremin_lumineux.ino
@@ -1,36 +1,68 @@
 /*
-    Thérémine lumineux
-  TP de la semaine 3 du MOOC "Fabriquer un objet connecté" 
- 
- Le montage :
- * Un piezo branché sur la broche 8
- * Une photorésistance branchée sur la broche A0 depuis +5V
- * Une résistance de 10kΩ branchée sur la broche A0 depuis GND
- 
- créé le 9 Avril 2014
- par Baptiste Gaultier
- 
- Ce code est en CC0 1.0 Universal
- 
- https://www.france-universite-numerique-mooc.fr/courses/MinesTelecom/04002/Trimestre_1_2014/about
-*/
+ * Thérémin lumineux
+ * Simulation d'un Thérémin lumineux pour le Mooc FUN/MinesTelecom Programmer un objet avec Arduino/TP03
+ *
+ * This code relased relased under GNU GENERAL PUBLIC LICENSE V3
+ * Consigne
+ * - Adapter AnalogInOutSerial pour que la fréquence du buzzer (branché sur la broche 8) soit proportionnelle à la luminosité mesurée par la photorésistance (branchée sur la broche 0)
+ * - Plus la luminosité reçue par la photorésistance est importante, plus la fréquence jouée par le buzzer sera élevée
+ * - Inversement, si on passe notre main devant le capteur de luminosité alors le son émis sera grave→fréquence basse.
+ * - Prendre une photo de votre circuit ou à prendre une capture d'écran du simulateur et à nous l'envoyer dans l'évaluation par les pairs.
+ *
+ * Matériel
+ * - Un Buzzer : Piézo éléctrique, mais aussi haut-parleur...
+ * - Un capteur : Photo éléctrique, remplaçable par un potentiomètre, un ultrason, Infrarouge, ...
+ *   - Une resistance de rappel de 10K Ohms
+ * - Personnelement j'ai ajouté :
+ *   - un intérupteur pour couper le buzzer (et préserver mes oreilles !)
+ *   - un potensiomètre avec la photorésistance pour ajuster la luminosité
+ *   - On pourait aussi avoir deux potentiomètres pour régler buzzMin et buzzMax à la volée !
+ *
+ * Montage
+ * - Broche 8 : Piezo branché entre la broche 8 et la masse
+ * - Broche A0 : Point Central reliant la photorésistance et la résistance de rappel.
+ *   - Photorésistance : Broche 5V et Point Central (vers la broche A0)
+ *   - Résistance de Rappel (10kΩ) : Broche GND et Point Central (vers la broche A0)
+ *
+ * Utilisation
+ * - Brancher le matériel, charger le programme, faites du bruit !
+ * - En modifiant les variables buzzMin et buzzMax, on peut réduire la plage des notes jouées, et donc transformer le bruit en musique.
+ */
 
+// Le Buzzer sur la broche 8
+const int buzzer = 8 ;
+// Fréquences MINimale et MAXimale du Buzzer
+// Dans les indices de la consigne, on donne entre 50 Hz et 30 000 Hz... Pour les oreilles on peut choisir plus agréable !
+// https://fr.wikipedia.org/w/index.php?title=Gamme_naturelle&section=18#Tableau_de_synth.C3.A8se
+int buzzMin = 50 ; // Pour un Do = 264
+// Fréquence maximale du Buzzer
+int buzzMax = 30000 ; // Pour 3 Ocatves plus haut = 3*buzzMin
+
+// La photoresistance sur la broche A0
+const int capteur = A0 ;
 // variable pour stocker la valeur reçue sur A0
 int sensorValue;
 
 void setup() {
+  // Initialise la communication avec l'ordinateur
+  Serial.begin(9600);
 }
 
 void loop() {
-  // lire la valeur de la photorésistance
-  // et stocker ça dans une variable
-  sensorValue = analogRead(A0);
+  // lire la valeur du capteur et la sauver dans sa variable.
+  sensorValue = analogRead(capteur);
 
-  // re étalonne la variable vers un grand intervalle de fréquences audibles
-  int pitch = map(sensorValue, 0, 1023, 50, 3000);
+  // Étalonnage de la valeur captée vers un intervalle de fréquences audibles
+  int pitch = map(sensorValue, 0, 1023, buzzMin, buzzMax);
 
-  // jouer la fréquence sur le piezo branché sur la broche 8
-  tone(8, pitch);
+  // Jouer la fréquence sur le buzzer
+  tone(buzzer, pitch);
+
+  // envoie tout ça vers l'ordinateur
+  Serial.print("Capteur = " );                       
+  Serial.print(sensorValue);      
+  Serial.print("\t Pitch = ");
+  Serial.println(pitch);
 
   // attendre 10 ms
   delay(10);
